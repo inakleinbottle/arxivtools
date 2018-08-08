8f343e15cf53775261d89cf5f2343693788bbd32
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2018-08-08 19:57:45 +0100

    Update entries.py
    Reverted definition of ArxivEntry to a named tuple rather than using a python 3.7 data class.

--- arxivtools/entries.py
+++ arxivtools/entries.py
@@ -1,13 +1,13 @@
 
-from dataclasses import dataclass
+from collections import namedtuple
 
 
+ArxivEntry = namedtuple('ArxivEntry', ('authors', 'arxiv_id', 'title', 'abstract'))
 
-
-@dataclass(frozen=True)
-class ArxivEntry:
-    authors: tuple
-    arxiv_id: str
-    title: str
-    abstract: str
-    #updated: str
+#@dataclass(frozen=True)
+#class ArxivEntry:
+#    authors: tuple
+#    arxiv_id: str
+#    title: str
+#    abstract: str
+#    #updated: str
