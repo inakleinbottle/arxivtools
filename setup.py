571da6fef16e370ed27b291fab1c62e64468d54b
inakleinbottle <41870650+inakleinbottle@users.noreply.github.com>
2018-08-28 22:23:49 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 setup(name='arxivtools',
-      version='0.0.2',
+      version='0.1.0',
       author='InAKleinBottle',
       packages=['arxivtools'],
       install_requires=['scipy',
